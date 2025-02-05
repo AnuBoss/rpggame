2b000f0dbbeeb658423bc66beb6848c21518283c
AnuBoss <Anuwat.sura@bumail.net>
2025-02-05 14:04:27 +0700

    Enemy

--- Assets/Scripts/Characters/Enemy.cs
+++ Assets/Scripts/Characters/Enemy.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Hero : Character
+public class Enemy : Character
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
