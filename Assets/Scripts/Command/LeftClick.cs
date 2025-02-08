4202feefa8d75f769c87e6588bf7b9863e8b28c4
AnuBoss <Anuwat.sura@bumail.net>
2025-02-08 14:09:53 +0700

    finish char ring

--- Assets/Scripts/Command/LeftClick.cs
+++ Assets/Scripts/Command/LeftClick.cs
@@ -25,6 +25,10 @@ public class LeftClick : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ClearEverything();
+        }
         if (Input.GetMouseButtonUp(0))
         {
             TrySelect(Input.mousePosition);
@@ -35,6 +39,11 @@ public class LeftClick : MonoBehaviour
     {
         curChar = hit.collider.GetComponent<Character>();
         Debug.Log("Selest Char: " + hit.collider.gameObject);
+
+        if (curChar != null)
+        {
+            curChar.ToggleRingSelection(true);
+        }
     }
 
     private void TrySelect(Vector2 screenPos)
@@ -52,5 +61,19 @@ public class LeftClick : MonoBehaviour
                     break;
             }
         }
+    }
+
+    private void ClearRingSelection()
+    {
+        if (curChar != null)
+        {
+            curChar.ToggleRingSelection(false);
+        }
+    }
+
+    private void ClearEverything()
+    {
+        ClearRingSelection();
+        curChar = null;
     }
 }
