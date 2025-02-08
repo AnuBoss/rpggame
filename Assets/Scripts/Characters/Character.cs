8e7769267bbcf076c37f5c66334ef763edbe1e8e
AnuBoss <Anuwat.sura@bumail.net>
2025-02-08 13:59:23 +0700

    Character ring

--- Assets/Scripts/Characters/Character.cs
+++ Assets/Scripts/Characters/Character.cs
@@ -21,8 +21,13 @@ public abstract class Character : MonoBehaviour
     [SerializeField]
     protected CharState state;
     public CharState State { get { return state; } }
-    
-    
+
+    [SerializeField] 
+    protected GameObject ringSelection;
+    public GameObject RingSelection
+    {
+        get { return ringSelection; }
+    }
 
     void Awake()
     {
@@ -60,6 +65,11 @@ public abstract class Character : MonoBehaviour
         {
             SetState(CharState.Idle);
         }
+    }
+
+    public void ToggleRingSelection(bool flag)
+    {
+        ringSelection.SetActive(flag);
     }
     
 
