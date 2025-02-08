c5529343df614d0e8eca14e2c3c3eced12a9326f
AnuBoss <Anuwat.sura@bumail.net>
2025-02-08 12:40:28 +0700

    LeftClick-update

--- Assets/Scripts/Command/LeftClick.cs
+++ Assets/Scripts/Command/LeftClick.cs
@@ -3,15 +3,36 @@ using UnityEngine;
 public class LeftClick : MonoBehaviour
 {
     public static LeftClick instance;
+
+    private Camera cam;
+
+    [SerializeField] 
+    private Character curChar;
+    public Character CurChar
+    {
+        get { return curChar; }
+    }
+
+    [SerializeField] 
+    private LayerMask layerMask;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         instance = this;
+        cam =Camera.main;
+        layerMask = LayerMask.GetMask("Ground", "Character", "Building", "Item");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void SelectCharacter(RaycastHit hit)
+    {
+        curChar = hit.collider.GetComponent<Character>();
+        Debug.Log("Selest Char: " + hit.collider.gameObject);
     }
 }
