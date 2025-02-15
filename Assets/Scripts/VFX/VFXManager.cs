8a3fc7bcc6ebe21f09d141165c2eca507add978f
AnuBoss <Anuwat.sura@bumail.net>
2025-02-15 12:07:41 +0700

    VFXManager script

--- Assets/Scripts/VFX/VFXManager.cs
+++ Assets/Scripts/VFX/VFXManager.cs
@@ -2,10 +2,17 @@ using UnityEngine;
 
 public class VFXManager : MonoBehaviour
 {
+    [SerializeField] private GameObject doubleRingMarker;
+    public GameObject DoubleRingMarker
+    {
+        get { return doubleRingMarker; }
+    }
+
+    public static VFXManager instance;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        instance = this;
     }
 
     // Update is called once per frame
