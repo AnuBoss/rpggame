7ff16cc6ea17084e1c9084b31c3157b2b864347c
AnuBoss <Anuwat.sura@bumail.net>
2025-02-22 13:12:44 +0700

    SelectionBox

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,7 +1,21 @@
+using System;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField] private RectTransform selectionBox;
+    public RectTransform SelectionBox
+    {
+        get { return selectionBox; }
+    }
+
+    public static UIManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
