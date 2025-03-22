47e43602fe387c095ea02e66bd65210e8514eacf
AnuBoss <Anuwat.sura@bumail.net>
2025-03-22 13:09:21 +0700

    itemdata

--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -1,16 +1,46 @@
 using UnityEngine;
 
-public class Item : MonoBehaviour
+public enum ItemType
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    Consumable,
+    Equipment,
+    Weapon,
+    Ammo,
+    Quest,
+    Other
+}
+
+[System.Serializable]
+public class Item 
+{
+    [SerializeField]
+    private int id;
+    public int ID { get { return id; } }
+
+    [SerializeField]
+    private string itemName;
+    public string ItemName { get { return itemName; } }
+
+    [SerializeField]
+    private ItemType type;
+    public ItemType Type { get { return type; } }
+
+    [SerializeField]
+    private Sprite icon;
+    public Sprite Icon { get { return icon; } }
+
+    [SerializeField]
+    private int power;
+    public int Power { get { return power; } }
+
+    public Item(ItemData data)
     {
-        
+        id = data.id;
+        itemName = data.itemName;
+        type = data.type;
+        icon = data.icon;
+        power = data.power;
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
